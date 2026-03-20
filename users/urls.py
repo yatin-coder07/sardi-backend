01af78c4401d28409bc8e1221a5032cb80b6a280
yatin sharma <yatins113@gmail.com>
2026-03-20 17:37:04 +0530

    admin views

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GoogleAuthView, UserDetailView
+from .views import GoogleAuthView, UserDetailView , FixAdminsView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("google/", GoogleAuthView.as_view(), name="google-login"),
     path("user/",UserDetailView.as_view(), name="user-detail"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("fix-admins/", FixAdminsView.as_view())
 ]
